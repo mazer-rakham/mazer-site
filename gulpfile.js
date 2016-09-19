2a8ae326f73d45f5d1ffcc228e0a749dfed0445b
Shawn Flock <sflock@navicamls.net>
2016-09-19 16:57:07 -0400

    commiting for home

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,25 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var connect = require('gulp-connect-php');
+gulp.task('sass', function(){
+    return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+        stream: true
+    }))
+});
+gulp.task('watch', ['browserSync', 'sass'],function(){
+    gulp.watch('app/scss/**/*.scss',['sass']);
+    gulp.watch('app/*.html')
+    gulp.watch('app/*.php')
+    gulp.watch('app/js/**/*.js')
+});
+gulp.task('browserSync', function(){
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+    });
+});
